b36491bddd2a04acdbb15acd7d51b7934db1d3e4
raihan-778 <raihanarif778@gmail.com>
2026-01-01 16:56:29 +0600

    loading and error page adde

--- components/Loading.jsx
+++ components/Loading.jsx
@@ -1,19 +1,18 @@
 "use client";
 
+import { RotatingSquare } from "react-loader-spinner";
+
 const LoadingSpinner = () => {
   return (
-    <div>
-      <h1>Loading ...</h1>
-    </div>
-    // <RotatingSquare
-    //   visible={true}
-    //   height="100"
-    //   width="100"
-    //   color="#10b981"
-    //   ariaLabel="rotating-square-loading"
-    //   wrapperStyle={{}}
-    //   wrapperClass=""
-    // />
+    <RotatingSquare
+      visible={true}
+      height="100"
+      width="100"
+      color="#10b981"
+      ariaLabel="rotating-square-loading"
+      wrapperStyle={{}}
+      wrapperClass=""
+    />
   );
 };
 
